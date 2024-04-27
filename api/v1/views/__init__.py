78040bf57968cbb2aee76f75c25f2891cc1d30c0
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-04-27 16:03:40 +0200

    update on task 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,8 @@
 """sharing app_views Blueprint"""
 
 from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,5 +11,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-
-app_views = Blueprint('app_views', __name__)
+# from api.v1.views.places_amenities import *
