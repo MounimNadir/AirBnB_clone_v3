d2506e1a9a2176453b3782b12e786d9858283702
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-04-28 09:58:06 +0200

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,7 +14,7 @@ def get_cities_by_state(state_id=None):
     """Retrieve list of all City objects of a State"""
     state = storage.get(State, state_id)
     if state is None:
-        abort(404)
+        return jsonify({"error": "Not found"}), 404
     cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
 
@@ -63,13 +63,14 @@ def create_city(state_id):
                  strict_slashes=False, methods=["PUT"])
 def update_city(city_id):
     """Update a city"""
+    city = storage.get(City, city_id)
+    if city is None:
+        return jsonify({"error": "Not found"}), 404
     if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    data = storage.get(City, city_id)
-    if data is None:
-        abort(404)
-    for key, value in request.get_json().items():
+        return jsonify({"error": "Not a JSON"}), 400
+    data = request.get_json()
+    for key, value in data.items():
         if key not in ['id', 'state_id', 'created_at', 'updated_at']:
-            setattr(data, key, value)
+            setattr(city, key, value)
     storage.save()
-    return jsonify(data.to_dict()), 200
+    return jsonify(city.to_dict()), 200
